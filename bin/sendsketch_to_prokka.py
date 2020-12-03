1d8dd96670a135c14b9246002e78177361913f6d
Emilio Rudbeck <emilir@rackham2.uppmax.uu.se>
2020-12-03 09:10:08 +0100

    Fixed typo in pythonscript and added temporary fix to Multiqc

--- bin/sendsketch_to_prokka.py
+++ bin/sendsketch_to_prokka.py
@@ -4,9 +4,9 @@ import sys
 sketch_file = sys.argv[1]
 stain_file = sys.argv[2]
 
-output_stain= "Not_in_list"
-output_species= "taxa"
-genus="Multiple"
+output_stain = "Not_in_list"
+output_species = "taxa"
+genus = "Multiple"
 
 with open(sketch_file, "r") as sketch:
 	sketch_lines = sketch.readlines()
@@ -19,12 +19,12 @@ with open(sketch_file, "r") as sketch:
 	genus_two = genus_two_line.split("\t")[11].split(" ")[0]
 
 	if len(genus_two) == 0 or genus_one == genus_two:
-		genus=genus_one
-		output_species=species_one.rstrip()
+		genus = genus_one
+		output_species = species_one.rstrip()
 		with open(stain_file, "r") as stain:
 			for line in stain:
 				if line.split("\t")[0] == genus:
-					output_stain= line.rstrip().split("\t")[1]
+					output_stain = line.rstrip().split("\t")[1]
 	else:
-		output_stain="Contaminated"
+		output_stain = "Contaminated"
 print(output_stain+"\t"+genus+"\t"+output_species)
