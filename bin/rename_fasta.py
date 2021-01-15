0d8d8b86bd1075134773b65b6ae29cd696d5d0ef
Fredrik Boulund <boulund@users.noreply.github.com>
2021-01-15 09:26:38 +0100

    Rewrite rename_fasta.py
    Untested!
    
    * Moved argument parsing to make module easier to import/reuse, added help message printout and graceful fail if run without arguments.
    * Removed uninformative argumentparser epilog message.
    * Removed outdated open/close semantics to use `with` statement instead.
    * Added logging, replaced print statements.

--- bin/rename_fasta.py
+++ bin/rename_fasta.py
@@ -1,44 +1,43 @@
 #!/usr/bin/env python3
-
 '''Rename contigs of a FASTA file with incremental count.'''
 
+from sys import argv, exit
 import argparse
+import logging
+
+logger = logging.getLogger(__name__)
 
 
-def main():
-    '''Execute renaming.'''
+def parse_args():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('-i', '--input', help='Path to input FASTA file', required=True)
+    parser.add_argument('-p', '--prefix', help='FASTA header prefix before integer count', type=str, default='')
+    parser.add_argument('-o', '--output', help='Path to output FASTA file', required=True)
+    
+    if len(argv) < 2:
+        parser.print_help()
+        exit(1)
+    
+    return parser.parse_args()
 
-    # Parse arguments.
-    parser = argparse.ArgumentParser(description='Rename FASTA files.', epilog='Work out those contigs.')
-    parser.add_argument('-i', '--input', help='indicate input FASTA file', required=True)
-    parser.add_argument('--pre', help='string pre contig count', type=str, default='')
-    parser.add_argument('-o', '--output', help='indicate output FASTA file', required=True)
-    args = parser.parse_args()
 
-    # Open FASTA.
-    fasta_in = open(args.input, 'r')
-
-    # Create FASTA output file.
-    fasta_out = open(args.output, 'w')
-
-    # Start counter.
+def main(input_filename, output_filename, header_prefix):
+    args = parse_args()
+    
+    logger.info('Processing {}...'.format(input_filename))
     count = 1
+    with open(input_filename, 'r') as fasta_in, open(output_filename, 'w') as fasta_out:
+        for line in fasta_in:
+            if line.startswith('>'):
+                contig_id = '>' + header_prefix + str(count) + '\n'
+                fasta_out.write(contig_id)
+                count += 1
+            else:
+                fasta_out.write(line)
 
-    # Parse file and write to output.
-    print('Parsing {}'.format(args.input))
-    for line in fasta_in.readlines():
-        if line.startswith('>'):
-            contig_id = '>' + args.pre + str(count) + '\n'
-            fasta_out.write(contig_id)
-            count += 1
-        else:
-            fasta_out.write(line)
-
-    # Finish.
-    fasta_out.close()
-    fasta_in.close()
-    print('Wrote {0} contigs to {1}.'.format(count, args.output))
+    logger.info('Wrote {0} contigs to "{1}".'.format(count, output_filename))
 
 
 if __name__ == '__main__':
-    main()
+    args = parse_args()
+    main(args.input, args.output, args.prefix)
